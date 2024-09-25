c57d57b7be1789fe79330b169e59cbdb37f30667
Loza Ubuntu <loza@usal.es>
2024-09-25 19:17:31 +0200

    Added science direct API. Query not working as expected

--- methodology-ia/3. Conduct the search/ResearchDatabases.py
+++ methodology-ia/3. Conduct the search/ResearchDatabases.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
+import time
 import requests
 import pandas as pd
 import urllib.parse
+import pandas as pd
+from tqdm import tqdm
 
 class BBDDSearchAPI(ABC):
     def __init__(self, api_key):
         self.api_key = api_key
 
     @abstractmethod
-    def search(self, query: str) -> 'pd.DataFrame':
+    def search(self, query: str, init_year:str, end_year:str, max_results:int) -> 'pd.DataFrame':
         '''
         Search in the database and return the results in a common format as pandas dataframe
         
@@ -21,7 +24,7 @@ class BBDDSearchAPI(ABC):
         pass
 
 class IEEEXploreSearchAPI(BBDDSearchAPI):
-    def search(self, query):
+    def search(self, query: str, init_year:str, end_year:str, max_results:int):
         # URL encode the query
         encoded_query = urllib.parse.quote(query)
 
@@ -49,22 +52,97 @@ class IEEEXploreSearchAPI(BBDDSearchAPI):
             return None
 
 class WebOfScienceSearchAPI(BBDDSearchAPI):
-    def search(self, query):
+    def search(self, query: str, init_year:str, end_year:str, max_results:int):
         url = f"https://api.webofscience.com/search?apikey={self.api_key}&query={query}"
         response = requests.get(url)
         return response.json()
 
 class SpringerLinkSearchAPI(BBDDSearchAPI):
-    def search(self, query):
+    def search(self, query: str, init_year:str, end_year:str, max_results:int):
         url = f"https://api.springer.com/search?apikey={self.api_key}&query={query}"
         response = requests.get(url)
         return response.json()
 
-class ScopusSearchAPI(BBDDSearchAPI):
-    def search(self, query):
-        url = f"https://api.elsevier.com/content/search/scopus?apikey={self.api_key}&query={query}"
-        response = requests.get(url)
-        return response.json()
+class ScienceDirectAPI(BBDDSearchAPI):
+    def search(self, query: str, init_year: str, end_year: str, max_results: int):
+        # Inicializa el array donde se almacenarán todos los resultados
+        all_entries = []
+
+        # Definir el tamaño del bloque (máximo por solicitud)
+        results_per_page = 100
+        offset = 0
+
+        # Mientras no tengamos todos los resultados
+        for offset in tqdm(range(0, max_results, results_per_page), desc="Buscando resultados", unit="página"):
+            # Construye el cuerpo de la solicitud con los filtros
+            body = {
+                "qs": query,  # Consulta de búsqueda
+                "pub": "",  # Dejar vacío si no se quiere limitar a una publicación específica
+                "filters": {
+                    "openAccess": False,  # Filtrar por acceso abierto (True/False)
+                    "date": {
+                        "from": f"{init_year}-01-01",  # Fecha de inicio
+                        "to": f"{end_year}-12-31"     # Fecha de fin
+                    },
+                },
+                "display": {
+                    "offset": offset,  # Desplazamiento para paginación
+                    "show":results_per_page,
+                }
+                
+            }
+
+            # Establece las cabeceras
+            headers = {
+                'X-ELS-APIKey': self.api_key,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+
+            # URL de la API de ScienceDirect Search V2
+            url = 'https://api.elsevier.com/content/search/sciencedirect'
+
+            # Envía la solicitud PUT
+            response = requests.put(url, json=body, headers=headers)
+
+            # Verifica si la solicitud fue exitosa
+            if response.status_code == 200:
+                data = response.json()
+                entries = data.get('results', [])
+
+                # Si no hay más resultados, salir del bucle
+                if not entries:
+                    break
+
+                for entry in entries:
+                    title = entry.get('title','')
+                    journal = entry.get('sourceTitle','')
+                    doi = entry.get('doi', '')
+                    publication_date = entry.get('publicationDate', '')
+
+                    # Añadir solo las entradas con DOI, título y fecha
+                    all_entries.append({
+                        'title': title,
+                        'doi': doi,
+                        'year': publication_date[:4] if publication_date != 'No date available' else 'No year available',
+                        'journal':journal
+                    })
+
+                # Incrementa el offset para la próxima solicitud
+                offset += results_per_page
+                # Esperar un segundo para no alcanzar los límites de la API
+                time.sleep(1)
+
+            else:
+                print(f'Error: {response.status_code}')
+                print(response.text)
+                break
+
+        # Retorna todos los resultados obtenidos en formato JSON
+        # Crear un DataFrame a partir de los registros
+        df_all_entries = pd.DataFrame(all_entries)
+    
+        return df_all_entries
 
 def get_search_api(db_type, api_key):
     if db_type == "ieee_xplore":
@@ -73,7 +151,7 @@ def get_search_api(db_type, api_key):
         return WebOfScienceSearchAPI(api_key)
     elif db_type == "springerlink":
         return SpringerLinkSearchAPI(api_key)
-    elif db_type == "scopus":
-        return ScopusSearchAPI(api_key)
+    elif db_type == "science_direct":
+        return ScienceDirectAPI(api_key)
     else:
         raise ValueError(f"Tipo de base de datos no soportado: {db_type}")
--- methodology-ia/3. Conduct the search/SearchInDatabases.py
+++ methodology-ia/3. Conduct the search/SearchInDatabases.py
@@ -1,6 +1,7 @@
-from ResearchDatabases import IEEEXploreSearchAPI, WebOfScienceSearchAPI, SpringerLinkSearchAPI, ScopusSearchAPI, get_search_api
+from ResearchDatabases import IEEEXploreSearchAPI, WebOfScienceSearchAPI, SpringerLinkSearchAPI, ScienceDirectAPI, get_search_api
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -8,10 +9,11 @@ def main():
 
     # DDBB and queries from brainstorming
     ddbb_queries = {
-        "ieee_xplore": "(\\\"llm\\\" OR \\\"large language model\\\" OR \\\"gpt\\\" OR \\\"llama\\\" OR \\\"mistral\\\") AND (\\\"recommender\\\" OR \\\"recsys\\\") AND (\\\"consumer hardware\\\" OR \\\"limitations\\\" OR \\\"issues\\\" OR \\\"efficient\\\" OR \\\"activity\\\")",
-        "web_of_science": "(\\\"llm\\\" OR \\\"large language model\\\" OR \\\"gpt\\\" OR \\\"llama\\\" OR \\\"mistral\\\") AND (\\\"recommender\\\" OR \\\"recsys\\\") AND (\\\"consumer hardware\\\" OR \\\"limitations\\\" OR \\\"issues\\\" OR \\\"efficient\\\" OR \\\"activity\\\")",
-        "springerlink": "(\\\"llm\\\" OR \\\"large language model\\\" OR \\\"gpt\\\" OR \\\"llama\\\" OR \\\"mistral\\\") AND (\\\"recommender\\\" OR \\\"recsys\\\") AND (\\\"consumer hardware\\\" OR \\\"limitations\\\" OR \\\"issues\\\" OR \\\"efficient\\\" OR \\\"activity\\\")",
-        "scopus": "(\\\"llm\\\" OR \\\"large language model\\\" OR \\\"gpt\\\" OR \\\"llama\\\" OR \\\"mistral\\\") AND (\\\"recommender\\\" OR \\\"recsys\\\") AND (\\\"consumer hardware\\\" OR \\\"limitations\\\" OR \\\"issues\\\" OR \\\"efficient\\\" OR \\\"activity\\\")"
+        "ieee_xplore": '("llm" OR "large language model" OR "gpt" OR "llama" OR "mistral") AND ("recommender" OR "recsys") AND ("consumer hardware" OR "limitations" OR "issues" OR "efficient" OR "activity")',
+        "web_of_science": '("llm" OR "large language model" OR "gpt" OR "llama" OR "mistral") AND ("recommender" OR "recsys") AND ("consumer hardware" OR "limitations" OR "issues" OR "efficient" OR "activity")',
+        "springerlink": '("llm" OR "large language model" OR "gpt" OR "llama" OR "mistral") AND ("recommender" OR "recsys") AND ("consumer hardware" OR "limitations" OR "issues" OR "efficient" OR "activity")',
+        "scopus": '("llm" OR "large language model" OR "gpt" OR "llama" OR "mistral") AND ("recommender" OR "recsys") AND ("consumer hardware" OR "limitations" OR "issues" OR "efficient" OR "activity")',
+        "science_direct": '"llm" and "recommender"'
     }
 
     # API keys
@@ -23,14 +25,30 @@ def main():
     }
 
     # Try first with ieee_xplore
-    db_type = "ieee_xplore"  # Cambia esto según el tipo de BBDD
+    db_type = "science_direct"  # Cambia esto según el tipo de BBDD
     # Get from a .env file the api_key
-    api_key = os.getenv("IEEE_XPLOR_API_KEY")
-    query = '("llm" OR "large language model" OR "gpt" OR "llama" OR "mistral") AND ("recommender" OR "recsys")'
-
+    api_key = os.getenv("SCOPUS_API_KEY")
+    query = ddbb_queries["science_direct"]
     search_api = get_search_api(db_type, api_key)
-    resultados = search_api.search(query)
-    print(resultados)
+    df_resultados = search_api.search(query,"2017","2025",1000)
+
+    # Deduplicar por DOI ()
+    # Eliminar duplicados por DOI
+    df_resultados_deduplicados = df_resultados.drop_duplicates(subset='doi', keep='first')
+    
+    # Guardar el DataFrame como CSV
+    df_resultados_deduplicados.to_csv('resultados_busqueda.csv', index=False)
+    
+    # Mostrar un resumen de la información del DataFrame por pantalla
+    print("\nResumen del DataFrame:")
+    print(f"Número total de resultados: {len(df_resultados_deduplicados)}")
+    print(f"Columnas: {', '.join(df_resultados_deduplicados.columns)}")
+    print("\nPrimeras 5 filas:")
+    print(df_resultados_deduplicados.head())
+    print("\nInformación estadística:")
+    print(df_resultados_deduplicados.describe(include='all'))
+    print("\nDistribución de años:")
+    print(df_resultados_deduplicados['year'].value_counts().sort_index())
 
 if __name__ == "__main__":
     main()
